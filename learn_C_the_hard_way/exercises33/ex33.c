3446c8e2ef9c621929e1ef3f3c59cc13f9f526ce
Hjw-Arch <hujinwei_121@163.com>
2024-10-31 17:32:38 +0800

    Auto commit by Makefile

--- learn_C_the_hard_way/exercises33/ex33.c
+++ learn_C_the_hard_way/exercises33/ex33.c
@@ -299,7 +299,7 @@ char *test_merge_sort()
     List *res2 = merge_sort(res);
     mu_assert(is_sorted(res), "Should still be sorted after merge sort.");
     List_destroy(res2);
-    List_destroy(res);
+    // List_destroy(res);
 
     // List_destroy(words);
     return NULL;
