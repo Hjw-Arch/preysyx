08617efcd55c72d814df67c6f66bf4a72ebb12d8
Hjw-Arch <hujinwei_121@163.com>
2024-10-31 11:45:24 +0800

    Auto commit by Makefile

--- git_autocommit.mk
+++ git_autocommit.mk
@@ -3,12 +3,20 @@
 # 自动执行 Git 操作的默认消息
 GIT_COMMIT_MSG ?= "Auto commit by Makefile"
 
+ROOT_DIR := ~/preysyx
+
 # 定义前置操作
 .PHONY: pre_git
 pre_git:
-	@git add .
-	@git commit -m "$(GIT_COMMIT_MSG)"
-	@git push
+	@cd $(ROOT_DIR) && git add .
+	@if [ -n "$$(git diff --cached)" ]; then \
+		echo "Committing changes..."; \
+		cd $(ROOT_DIR) && git commit -m "$(GIT_COMMIT_MSG)"; \
+		cd $(ROOT_DIR) && git push; \
+	else \
+		echo "No changes to commit."; \
+	fi
+	@cd $(CURDIR) # 确保回到初始目录
 
 # 当任何目标被执行时，首先调用 pre_git
 # 使用 MAKECMDGOALS 遍历所有目标
